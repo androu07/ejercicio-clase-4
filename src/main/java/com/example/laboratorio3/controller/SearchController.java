9b4a8b97d11e923d1e09aa2f25819beb882b6dd8
Yosthim <a20201497@pucp.edu.pe>
2024-04-19 20:53:18 -0500

    Fragmento en indice.html y adicion de repPaisCiudad.html

--- src/main/java/com/example/laboratorio3/controller/SearchController.java
+++ src/main/java/com/example/laboratorio3/controller/SearchController.java
@@ -1,15 +1,23 @@
 package com.example.laboratorio3.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 //COMPLETAR
+@RequestMapping(value = "/reportes", method = RequestMethod.GET)
 public class SearchController {
-
-    //COMPLETAR
 
     @GetMapping(value = {"","/"})
     public String indice(){
         return "Search/indice";
+    }
+
+    @GetMapping(value = "/reportePaisCiudad")
+    public String reportePais() {
+        return "Search/repPaisCiudad";
     }
 
 
