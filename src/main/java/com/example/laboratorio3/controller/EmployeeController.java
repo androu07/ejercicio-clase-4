645412271a1004b29c976eca94e28398d64e7047
andres <a20191450@pucp.edu.pe>
2024-04-19 20:35:48 -0500

    1

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,26 +1,48 @@
 package com.example.laboratorio3.controller;
 
-//COMPLETAR
+import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
 public class EmployeeController {
 
-    //COMPLETAR
-
-    public String listaEmployee(   ){
-        //COMPLETAR
+    final
+    EmployeesRepository employeesRepository;
+    public EmployeeController(EmployeesRepository employeesRepository) {
+        this.employeesRepository = employeesRepository;
     }
 
+    @GetMapping("/empleados")
+    public String listaEmployee(Model model){
+        List<Employees> lista = EmployeesRepository.findAll();
+        model.addAttribute("listaEmpleados",lista);
+        return "employee/lista";
+    }
+
+    @GetMapping("/empleados/new")
     public String nuevoEmployeeForm( ) {
-        //COMPLETAR
+        return "employee/newFrm";
     }
 
-
-    public String guardarEmployee() {
-        //COMPLETAR
+    @PostMapping("/guardar")
+    public String guardarEmployee(Employees employees) {
+        employeesRepository.save(employees);
+        return "redirect:/employee/lista";
     }
 
-
-    public String editarEmployee() {
-        //COMPLETAR
+    @GetMapping("/editar")
+    public String editarEmployee(Model model) {
+        @RequestParam("id") int id; {
+            Optional<Employees> optionalEmployees =
+        }
     }
 
 
--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -5,8 +5,63 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
 import java.util.Date;
 
-//COMPLETAR
+@Entity
+@Table(name = "employees")
 public class Employees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id", nullable = false)
+    private int employeeid;
 
-           //COMPLETAR
+    @Column(name = "first_name", nullable = false)
+    private String first_name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastname;
+
+    @Column(name = "job_id", nullable = false)
+    private int jobid;
+
+    @Column(name = "department_id", nullable = false)
+    private int department_id;
+
+    public int getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(int employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getJobid() {
+        return jobid;
+    }
+
+    public void setJobid(int jobid) {
+        this.jobid = jobid;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
 }
